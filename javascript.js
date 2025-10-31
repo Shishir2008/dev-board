af0a53d4fb9dd7d0cd13f0a67698167b7ec455eb
Shishir2008 <shishir2008@example.com>
2025-10-31 10:02:03 +0600

    add icon and more javascript code

--- javascript.js
+++ javascript.js
@@ -7,6 +7,8 @@ document.getElementById('Blog-click').addEventListener('click', function () {
 
 
 const quantityElement = document.querySelectorAll('.main-btn');
+const cardContenar = document.getElementById('card-contenar');
+let array = [];
 for (let btn of quantityElement) {
     btn.addEventListener('click', function (event) {
         alert('Bord Updedt Successfully')
@@ -24,5 +26,22 @@ for (let btn of quantityElement) {
 
         quantityElement.innerText = newQuantity;
         quantityElements.innerText = sameNewQuantity;
+        
+        btn.style.background = 'white';
+        btn.style.color = 'gray';
+        btn.style.cursor = 'not-allowed';
+        btn.style.pointerEvents = 'none';
+
+
+        const p = document.createElement('p');
+        p.classList.add('bg-[#F4F7FF]','rounded-md','p-2','my-3','text-gray-400','font-semibold')
+        p.innerText = 'You have Complete The Task Add Dark Mode at 12:48:15 PM';
+        cardContenar.appendChild(p);
+
     })
 }
+
+
+document.getElementById('hidde-btn').addEventListener('click',function(){
+    cardContenar.classList.add('hidden');
+})
